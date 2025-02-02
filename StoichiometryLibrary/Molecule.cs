7ee93b4386be2819f83127a16565dee75e8fe597
Ayden Nicholson <a_nicholson184719@fanshaweonline.ca>
2025-02-02 14:00:18 -0500

    Molecule Class Built - No Logic Implementation

--- StoichiometryLibrary/Molecule.cs
+++ StoichiometryLibrary/Molecule.cs
@@ -8,5 +8,38 @@ namespace StoichiometryLibrary
 {
     public class Molecule
     {
+        // Properties
+        public bool Valid { get; }
+        public string Formula { get; set; }
+
+        // Default Constructor
+        public Molecule()
+        {
+            Formula = "";
+        }
+
+        // One-Arg Constructor
+        public Molecule(string formula)
+        {
+            Formula = formula;
+        }
+
+        // Method that returns a double that is the total mass of the molecule described by Formula, if valid
+        public double CalcMass()
+        {
+            // Throw Exception if formula is not valid
+            if (!Valid) { throw new InvalidOperationException("Formula is Invalid."); }
+
+            return 0.0;
+        }
+
+        // Method returning an array of IMolecularElement interfaces containing one item for each element found in Formula, if valid
+        public IMolecularElement[] GetComposition()
+        {
+            // Throw Exception if formula is not valid
+            if (!Valid) { throw new InvalidOperationException("Formula is Invalid."); }
+
+            return new IMolecularElement[0];
+        }
     }
 }
